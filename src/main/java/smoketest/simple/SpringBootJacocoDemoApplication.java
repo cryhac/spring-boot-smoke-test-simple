9fb2b23c0b09287df90cc5e34f4e4b4da1bc5feb
will <wu.liangxing@towngas.com.cn>
2024-07-31 23:30:03 +0800

    first commit

---
+++ src/main/java/smoketest/simple/SpringBootJacocoDemoApplication.java
@@ -0,0 +1,11 @@
+package smoketest.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJacocoDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJacocoDemoApplication.class, args);
+	}
+}
